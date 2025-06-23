548902a6ba67a9488de89ac15e0c93a30cb650f7
Raiden <raiden.liu@konec.com.au>
2025-06-23 20:29:13 +0800

    发布库

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id("com.android.library")
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     `maven-publish`
@@ -10,11 +10,10 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.raiden.lcalendar"
+        publishing { singleVariant("release") }
         minSdk = 30
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
